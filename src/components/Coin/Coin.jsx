b6e55191f4a60013ef53cfe4e23d357965caef38
TheMightyYak1 <78634345+TheMightyYak1@users.noreply.github.com>
2022-02-17 17:38:56 +1000

    lifting state up

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -17,11 +17,9 @@ const Td = styled.td`
 export default class Coin extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            price: this.props.price
-        }
         this.handleClick = this.handleClick.bind(this);
-    }
+        }
+    
     /*
     componentDidMount(){
         const callback = () => {
@@ -37,9 +35,13 @@ export default class Coin extends Component {
         setInterval(callback, 1000);
     }
     */
-    handleClick(event){
+    handleClick(e){
         //prevent default action of submitting form
-        event.preventDefault();
+        e.preventDefault();
+        this.props.handleRefresh(this.props.ticker);
+
+    }
+    /*    
         const randomPercentage = 0.995 + Math.random() * 0.01;
         this.setState(function(oldState){
             return {
@@ -47,13 +49,13 @@ export default class Coin extends Component {
             }
         })
     }
-
+*/
   render() {
     return (
         <tr>
             <Td>{this.props.name}</Td>
             <Td>{this.props.ticker}</Td>
-            <Td>${this.state.price}</Td>
+            <Td>$ {this.props.price}</Td>
             <Td>
                 <form action="$" method="POST">
                     <button onClick={this.handleClick}>Refresh</button>
@@ -62,8 +64,9 @@ export default class Coin extends Component {
             </Td>
         </tr>
     );
-  }
+    }
 }
+
 
 Coin.propTypes = {
     name: PropTypes.string.isRequired,

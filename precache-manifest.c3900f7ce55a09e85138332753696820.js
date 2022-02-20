5553a0a6d8d40be82ac1e99d3133d8b199ac1150
TheMightyYak1 <78634345+TheMightyYak1@users.noreply.github.com>
2022-02-20 17:06:44 +1000

    Updates

---
+++ precache-manifest.c3900f7ce55a09e85138332753696820.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7e25eb4981feed3ac1fed2403f9266",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "59550c50c3c7107cbda9",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2ef5280358f0dcb15d7",
+    "url": "/coin-exchange/static/js/2.f0e1afce.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/coin-exchange/static/js/2.f0e1afce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6d82f8bf258a9309ae",
+    "url": "/coin-exchange/static/js/3.52490744.chunk.js"
+  },
+  {
+    "revision": "59550c50c3c7107cbda9",
+    "url": "/coin-exchange/static/js/main.edcc5ff1.chunk.js"
+  },
+  {
+    "revision": "bd8ccaff6c30733a5d58",
+    "url": "/coin-exchange/static/js/runtime-main.751a7d68.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/coin-exchange/static/media/logo.06e73328.svg"
+  }
+]);
